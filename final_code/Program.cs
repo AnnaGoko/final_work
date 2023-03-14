0660e55e8dacf2bf701a42ee577758a4d3c4c24c
AnnaGoko <anna.gordienko@myrambler.ru>
2023-03-15 02:23:07 +0300

    add: get array's elements from the console

--- final_code/Program.cs
+++ final_code/Program.cs
@@ -1,4 +1,22 @@
-﻿string[] FirstArray = new string [] { "Tom", "Pit", "Posa", "Lisa", "Ann", "Kate", "Loid", "MJ" };
+﻿string[] FirstArray = new string[100];
+            int element = 0; 
+            string test;
+            int count = 0;
+ 
+            do
+            {
+                test = Console.ReadLine();
+                if (test == "end") break;
+                FirstArray[element] = (test);
+                ++element; ++count;
+            } while (true);
+            //for (int j = 0; j < count; j++)
+            //{
+            //    Console.Write("{0} ",FirstArray[j]);
+            //}
+            Console.ReadLine();
+
+//string[] FirstArray = new string [] { "Tom", "Pit", "Posa", "Lisa", "Ann", "Kate", "Loid", "MJ" };
 string[] SecondArray = new string [0];
 
 for (int i = 0; i < FirstArray.Length; i++)
