24616bf1c19b42204951f6828c2dfd545da4ed66
AnnaGoko <anna.gordienko@myrambler.ru>
2023-03-15 02:16:13 +0300

    add: algorithm outputs right elements from the array

--- final_code/Program.cs
+++ final_code/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? a = Console.ReadLine();
+string[] FirstArray = new string [] {  "ete", "rdfs"};
+string[] SecondArray = new string [0];
+//Console.WriteLine (FirstArray);
+//PrintArray (FirstArray);
+//void PrintArray(string [] FirstArray ){
+
+for (int i = 0; i < FirstArray.Length; i++)
+{
+    if ( FirstArray[i].Length <= 3){
+    Console.Write(FirstArray[i] + " ");
+    Array.Resize(ref SecondArray, SecondArray.Length + 1);
+    for (int j = 0;j < SecondArray.Length;j++)
+    {
+        
+    }
+    //string[]SecondArray = new string [FirstArray.Length + 1];
+
+}
+}
