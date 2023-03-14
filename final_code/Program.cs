4917191c4d5c711e29ea6546c5c64c5945a61fd5
AnnaGoko <anna.gordienko@myrambler.ru>
2023-03-15 02:25:38 +0300

    fix: optimize the code

--- final_code/Program.cs
+++ final_code/Program.cs
@@ -1,25 +1,17 @@
-﻿string[] FirstArray = new string[100];
-            int element = 0; 
-            string test;
-            int count = 0;
- 
-            do
-            {
-                test = Console.ReadLine();
-                if (test == "end") break;
-                FirstArray[element] = (test);
-                ++element; ++count;
-            } while (true);
-            //for (int j = 0; j < count; j++)
-            //{
-            //    Console.Write("{0} ",FirstArray[j]);
-            //}
-            Console.ReadLine();
-
-//string[] FirstArray = new string [] { "Tom", "Pit", "Posa", "Lisa", "Ann", "Kate", "Loid", "MJ" };
+﻿string[] FirstArray = new string[1];
 string[] SecondArray = new string [0];
+    int index = 0; 
+    string element;
+ 
+do
+{
+    element = Console.ReadLine() ?? "Null";
+    if (element == "stop") break;
+    FirstArray[index] = (element) ?? "Null";
+    ++index; Array.Resize(ref FirstArray, FirstArray.Length + 1);
+} while (true);
 
-for (int i = 0; i < FirstArray.Length; i++)
+for (int i = 0; i < FirstArray.Length - 1; i++)
 {
     if ( FirstArray[i].Length <= 3){
     Array.Resize(ref SecondArray, SecondArray.Length + 1);
