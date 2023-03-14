89b8e5615cd465ee007e4ce6bde57fde2214145f
AnnaGoko <anna.gordienko@myrambler.ru>
2023-03-15 02:21:05 +0300

    fix: optimize the code

--- final_code/Program.cs
+++ final_code/Program.cs
@@ -1,20 +1,12 @@
-﻿Console.WriteLine("Введите что-то: "); 
-string? a = Console.ReadLine();
-
-string[] FirstArray = new string [] {  "ete", "rdfs", "zf", "dsfd", "dfs"};
+﻿string[] FirstArray = new string [] { "Tom", "Pit", "Posa", "Lisa", "Ann", "Kate", "Loid", "MJ" };
 string[] SecondArray = new string [0];
 
 for (int i = 0; i < FirstArray.Length; i++)
 {
     if ( FirstArray[i].Length <= 3){
-    //Console.Write(FirstArray[i] + " ");
     Array.Resize(ref SecondArray, SecondArray.Length + 1);
     SecondArray[SecondArray.Length - 1] = FirstArray[i];
     }   
 }
 
-PrintArray(SecondArray);
-
-void PrintArray(string [] SecondArray){
-    Console.WriteLine(string.Join( ", " , SecondArray));
-}
+Console.WriteLine(string.Join( ", " , SecondArray));
